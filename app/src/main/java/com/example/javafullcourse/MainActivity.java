618daa90d44f79362feb61a1db1b4affacbf28de
riccigamiao3076 <n01363076@humbermail.ca>
2023-01-09 10:50:17 -0500

    User Input button with Toast message added

--- app/src/main/java/com/example/javafullcourse/MainActivity.java
+++ app/src/main/java/com/example/javafullcourse/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.javafullcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button userInputButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInputButton = findViewById(R.id.inputButton);
+
+        userInputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "User Input", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
 }
