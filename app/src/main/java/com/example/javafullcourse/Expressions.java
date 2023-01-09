63c05f7d5ce0ee794f93f0e336916054569efff4
riccigamiao3076 <n01363076@humbermail.ca>
2023-01-09 12:30:32 -0500

    EditText fields, Spinner with items, Button and TextView are added

--- app/src/main/java/com/example/javafullcourse/Expressions.java
+++ app/src/main/java/com/example/javafullcourse/Expressions.java
@@ -2,12 +2,35 @@ package com.example.javafullcourse;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class Expressions extends Activity {
+public class Expressions extends Activity implements AdapterView.OnItemSelectedListener {
+
+    Spinner opSpinner;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expressions);
+
+        opSpinner = findViewById(R.id.operatorsSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.operators, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        opSpinner.setAdapter(adapter);
+        opSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
