f27ece15c48b293e4fcd37d6aacb23ffbd84f457
riccigamiao3076 <n01363076@humbermail.ca>
2023-01-10 13:45:08 -0500

    Methods to check if input/s is/are empty are created

--- app/src/main/java/com/example/javafullcourse/MathClass.java
+++ app/src/main/java/com/example/javafullcourse/MathClass.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MathClass extends Activity implements View.OnClickListener {
 
-    EditText firstValue, secondValue;
+    EditText firstValue, secondValue, aValue, bValue;
     Button max, min, ceil, floor, abs, sqrt, round, hypo;
-    String getFV, getSV;
+    TextView displayAns, displayHypo;
+    String getFV, getSV, getSideA, getSideB;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,8 @@ public class MathClass extends Activity implements View.OnClickListener {
 
         firstValue = findViewById(R.id.eTextNumber1);
         secondValue = findViewById(R.id.eTextNumber2);
+        aValue = findViewById(R.id.sideNum1);
+        bValue = findViewById(R.id.sideNum2);
 
         max = findViewById(R.id.maxButton);
         min = findViewById(R.id.minButton);
@@ -28,7 +32,7 @@ public class MathClass extends Activity implements View.OnClickListener {
         abs = findViewById(R.id.absButton);
         sqrt = findViewById(R.id.sqrtButton);
         round = findViewById(R.id.roundButton);
-        hypo = findViewById(R.id.roundButton);
+        hypo = findViewById(R.id.hypoButton);
 
         max.setOnClickListener(this);
         min.setOnClickListener(this);
@@ -45,33 +49,68 @@ public class MathClass extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         getFV = firstValue.getText().toString();
         getSV = secondValue.getText().toString();
+        getSideA = aValue.getText().toString();
+        getSideB = bValue.getText().toString();
 
         //Switch case for all buttons
         switch (v.getId()) {
             case  R.id.maxButton:
                 Toast.makeText(this, "Maximum number", Toast.LENGTH_SHORT).show();
+                missingValueChecker(getFV, getSV);
                 break;
             case R.id.minButton:
                 Toast.makeText(this, "Minimum number", Toast.LENGTH_SHORT).show();
+                missingValueChecker(getFV, getSV);
                 break;
             case R.id.ceilButton:
                 Toast.makeText(this, "Round UP number", Toast.LENGTH_SHORT).show();
+                firstValueChecker(getFV, getSV);
                 break;
             case R.id.floorButton:
                 Toast.makeText(this, "Round DOWN number", Toast.LENGTH_SHORT).show();
+                firstValueChecker(getFV, getSV);
                 break;
             case R.id.absButton:
                 Toast.makeText(this, "Absolute number", Toast.LENGTH_SHORT).show();
+                firstValueChecker(getFV, getSV);
                 break;
             case R.id.sqrtButton:
                 Toast.makeText(this, "Square Root number", Toast.LENGTH_SHORT).show();
+                firstValueChecker(getFV, getSV);
                 break;
             case R.id.roundButton:
                 Toast.makeText(this, "Round number", Toast.LENGTH_SHORT).show();
+                firstValueChecker(getFV, getSV);
                 break;
             case R.id.hypoButton:
                 Toast.makeText(this, "Hypotenuse number", Toast.LENGTH_SHORT).show();
+                hypoValueChecker(getSideA, getSideB);
                 break;
         }
     }
+
+    public void missingValueChecker(String fValue, String sValue) {
+        if (fValue.isEmpty()) {
+            firstValue.setError("Empty");
+
+        } if (sValue.isEmpty()) {
+            secondValue.setError("Empty");
+        }
+    }
+
+    public void firstValueChecker(String fValue, String sValue) {
+        if (fValue.isEmpty()) {
+            firstValue.setError("Empty");
+        }
+    }
+
+    public void hypoValueChecker(String fValue, String sValue) {
+        if (fValue.isEmpty()) {
+            aValue.setError("Empty");
+
+        } else if (sValue.isEmpty()) {
+            bValue.setError("Empty");
+        }
+    }
+
 }
